64b963b086eaa1934a3f8bc85439433e0fac20d6
jae-0 <wodud0747@naver.com>
2023-11-24 16:40:41 +0900

    nameCardInfo Class 추가

---
+++ app/src/main/java/com/example/insquare/nameCardInfo.java
@@ -0,0 +1,40 @@
+package com.example.insquare;
+
+public class nameCardInfo extends Register{
+    private String company;
+    private String department;
+    private String position;
+
+    private String address;
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
